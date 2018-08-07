46633fd297b7fcac6ccb2ed04e48f5ea63bcc802
Tyler Wong <ursatechie@gmail.com>
2018-08-06 17:17:28 -0700

    master add styled components; gsap - playing around

---
+++ src/components/animation.js
@@ -0,0 +1,11 @@
+import { TweenLite, Elastic } from 'gsap'
+
+const duration = 0.5
+
+const HelloText = () => (
+	<div>
+		<div>{duration}</div>
+	</div>
+)
+
+export default HelloText
--- src/pages/ui.js
+++ src/pages/ui.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	background: pink;
+	margin: 0 auto;
+	max-width: 640px;
+
+	a {
+		color: white;
+	}
+`
+
+const Header = styled.header`
+	align-items: center;
+	background: rebeccapurple;
+	color: white;
+	display: flex;
+	padding: 20px;
+	height: 30px;
+`
+
+const Body = styled.div`
+	background: #eee;
+	height: 400px;
+	padding: 20px;
+`
+
+const Footer = styled.div`
+	align-items: center;
+	background: #333;
+	color: white;
+	display: flex;
+	height: 100px;
+	padding: 20px;
+`
 
 const UIPage = () => (
-	<div>
-		<Link to="/">Go back to the homepage</Link>
-		<h1>UI Toolkit</h1>
-	</div>
+	<Wrapper>
+		<Header>
+			<Link to="/">Go back to the homepage</Link>
+		</Header>
+
+		<Body>
+			<h1>UI Toolkit</h1>
+		</Body>
+
+		<Footer>Footer Content</Footer>
+	</Wrapper>
 )
 
 export default UIPage
