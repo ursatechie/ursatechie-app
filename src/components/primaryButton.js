2a45bda536cb76f5eb7a9643353e9d8f9e2b4342
Tyler Wong <ursatechie@gmail.com>
2018-08-14 15:03:57 -0700

    master add test primarybutton which take out later

---
+++ src/components/primaryButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const PrimaryButton = ({ siteTitle }) => (
+	<button class="primary-button">Click me</button>
+)
+
+export default PrimaryButton
