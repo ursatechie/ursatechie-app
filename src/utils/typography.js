398cf89f01e74199041fd862bdcbf9cc72eb0025
Tyler Wong <ursatechie@gmail.com>
2018-08-14 16:22:10 -0700

    master update typography Roboto Slab and Roboto

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,25 @@
 import Typography from 'typography'
+// import oceanBeachTheme from 'typography-theme-ocean-beach'
 
-const typography = new Typography({ baseFontSize: '14px' })
+// https://kyleamathews.github.io/typography.js/
+// const typography = new Typography({ baseFontSize: '14px' })
+// oceanBeachTheme.baseFontSize = '14px'
+// const typography = new Typography(oceanBeachTheme)
+
+const typography = new Typography({
+	baseFontSize: '18px',
+	baseLineHeight: 1.666,
+	headerFontFamily: [
+		'Roboto Slab',
+		'Roboto',
+		'Avenir Next',
+		'Helvetica Neue',
+		'Segoe UI',
+		'Helvetica',
+		'Arial',
+		'sans-serif',
+	],
+	bodyFontFamily: ['Roboto', 'Georgia', 'serif'],
+})
 
 export default typography
