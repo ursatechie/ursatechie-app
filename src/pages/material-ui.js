5424af738dcdef2dcdc14f05bf7264d02ba5d4b9
Tyler Wong <ursatechie@gmail.com>
2018-08-28 16:52:42 -0700

    master updated material page to just use the button alone

--- src/pages/material-ui.js
+++ src/pages/material-ui.js
@@ -1,72 +1,11 @@
-// import React from 'react'
-// // import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
 
-// // function App() {
-// // 	return (
-// // 		<Button variant="raised" color="primary">
-// // 			Hello World
-// // 		</Button>
-// // 	)
-// // }
-
-// const MaterialUIPage = () => <div>hello</div>
-
-// export default MaterialUIPage
-
-// // ReactDOM.render(<App />, document.querySelector('#app'))
-
 import React from 'react'
-import Link from 'gatsby-link'
-import styled from 'styled-components'
 
-const Wrapper = styled.div`
-	background: pink;
-	margin: 0 auto;
-	max-width: 640px;
-
-	a {
-		color: white;
-	}
-`
-
-const Header = styled.header`
-	align-items: center;
-	background: rebeccapurple;
-	color: white;
-	display: flex;
-	padding: 20px;
-	height: 30px;
-`
-
-const Body = styled.div`
-	background: #eee;
-	height: 400px;
-	padding: 20px;
-`
-
-const Footer = styled.div`
-	align-items: center;
-	background: #333;
-	color: white;
-	display: flex;
-	height: 100px;
-	padding: 20px;
-`
-
-const UIPage = () => (
-	<Wrapper>
-		<Button>test</Button>
-		<Header>
-			<Link to="/">Go back to the homepage</Link>
-		</Header>
-
-		<Body>
-			<h1>UI Toolkit</h1>
-		</Body>
-
-		<Footer>Footer Content</Footer>
-	</Wrapper>
+const MaterialUIPage = () => (
+	<Button variant="raised" color="primary">
+		Hello World
+	</Button>
 )
 
-export default UIPage
+export default MaterialUIPage
