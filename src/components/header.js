e4c54d8bdb2a97d6f91a1236301716729f35d598
Tyler Wong <ursatechie@gmail.com>
2018-08-29 15:30:01 -0700

    master conversion of header const to class - need to grab { siteTitle } from this.props instead of just passing it in

--- src/components/header.js
+++ src/components/header.js
@@ -8,17 +8,46 @@ import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 
-const Header = ({ siteTitle }) => (
-	<AppBar position="static" color="default">
-		<Toolbar>
-			<IconButton color="inherit" aria-label="Menu">
-				<MenuIcon />
-			</IconButton>
-			<Typography variant="title" color="inherit">
-				{siteTitle}
-			</Typography>
-		</Toolbar>
-	</AppBar>
-)
+import Button from '@material-ui/core/Button'
+
+class Header extends React.Component {
+	toggleDrawer = (side, open) => () => {
+		this.setState({
+			[side]: open,
+		})
+	}
+
+	render() {
+		const { siteTitle } = this.props
+
+		return (
+			<AppBar position="static" color="default">
+				<Toolbar>
+					<IconButton color="inherit" aria-label="Menu">
+						<MenuIcon />
+						<Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>
+					</IconButton>
+					<Typography variant="title" color="inherit">
+						{siteTitle}
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		)
+	}
+}
+
+// const Header = ({ siteTitle }) => (
+// 	<AppBar position="static" color="default">
+// 		<Toolbar>
+// 			<IconButton color="inherit" aria-label="Menu">
+// 				<MenuIcon />
+// 				<Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>
+// 			</IconButton>
+// 			<Typography variant="title" color="inherit">
+// 				{siteTitle}
+// 			</Typography>
+// 		</Toolbar>
+// 	</AppBar>
+// )
 
 export default Header
