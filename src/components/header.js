6ef956749dc677069660c2dd3997ccd82fbe643c
Tyler Wong <ursatechie@gmail.com>
2018-08-29 15:23:54 -0700

    master add menu icon to the app bar

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,15 @@ import styled from 'styled-components'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const Header = ({ siteTitle }) => (
 	<AppBar position="static" color="default">
 		<Toolbar>
+			<IconButton color="inherit" aria-label="Menu">
+				<MenuIcon />
+			</IconButton>
 			<Typography variant="title" color="inherit">
 				{siteTitle}
 			</Typography>
