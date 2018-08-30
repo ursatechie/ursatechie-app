e2cd10550e547676bc89701129b490004d38161c
Tyler Wong <ursatechie@gmail.com>
2018-08-29 17:31:52 -0700

    master hook up links to drawer; still need to style them

--- src/components/tileData.js
+++ src/components/tileData.js
@@ -10,20 +10,27 @@ import ListItemText from '@material-ui/core/ListItemText'
 import InboxIcon from '@material-ui/icons/Inbox'
 import DraftsIcon from '@material-ui/icons/Drafts'
 
+import Link from 'gatsby-link'
+
 const MyDrawerItems = () => (
 	<React.Fragment>
-		<ListItem button>
-			<ListItemIcon>
-				<InboxIcon />
-			</ListItemIcon>
-			<ListItemText primary="Inbox" />
-		</ListItem>
-		<ListItem button>
-			<ListItemIcon>
-				<DraftsIcon />
-			</ListItemIcon>
-			<ListItemText primary="Drafts" />
-		</ListItem>
+		<Link to="/">
+			<ListItem button>
+				<ListItemIcon>
+					<InboxIcon />
+				</ListItemIcon>
+				<ListItemText primary="Home" />
+			</ListItem>
+		</Link>
+
+		<Link to="/typographypage/">
+			<ListItem button>
+				<ListItemIcon>
+					<DraftsIcon />
+				</ListItemIcon>
+				<ListItemText primary="Typography" />
+			</ListItem>
+		</Link>
 	</React.Fragment>
 )
 
