8d7340205e01c5e09c92a6925b6c88d25008bf08
Tyler Wong <ursatechie@gmail.com>
2018-09-28 14:00:28 -0700

    master crazy attempt to try to get images in

--- src/components/gridListComp.js
+++ src/components/gridListComp.js
@@ -10,6 +10,57 @@ import IconButton from '@material-ui/core/IconButton'
 import InfoIcon from '@material-ui/icons/Info'
 // import data.site.siteMetadata.projects from './tileData'
 
+// const images = require.context('../images', true)
+// const imagePath = name => images(name, true)
+
+// then within your component
+// console.log(imagePath('./jump.png')) // it's relative to context
+
+// import * as images from '../images'
+import awesomeImg from '../images/jump.jpg'
+
+function importAll(r) {
+	let images = {}
+	r.keys().map((item, index) => {
+		images[item.replace('./', '')] = r(item)
+	})
+	return images
+}
+
+const images = importAll(
+	require.context('../images', false, /\.(png|jpe?g|svg)$/)
+)
+
+var projects = {
+	projects: [
+		{
+			img: images['jump.jpg'],
+			title: 'title',
+			author: 'author',
+		},
+		{
+			img: images['jump.jpg'],
+			title: 'title',
+			author: 'author',
+		},
+		{
+			img: '31asdfasdfas',
+			title: 'title',
+			author: 'author',
+		},
+		{
+			img: '41asdfasdfas',
+			title: 'title',
+			author: 'asdfs',
+		},
+		{
+			img: '51asdfasdfas',
+			title: 'title',
+			author: 'author',
+		},
+	],
+}
+
 const TGridListWrapper = styled.div`
 	background-color: var(--white);
 	display: flex;
@@ -35,7 +86,8 @@ const GridListComp = ({ projects }) => (
 
 			{projects.map(project => (
 				<GridListTile key={project.img}>
-					<img src={project.img} alt={project.title} />
+					<img src={projects[0].img} alt={project.title} />
+					<img src={awesomeImg} alt={project.title} />
 					<GridListTileBar
 						title={project.title}
 						subtitle={<span>by: {project.author}</span>}
---
+++ src/pages/image.js
@@ -0,0 +1,111 @@
+// // https://itnext.io/writing-customizable-styled-react-components-51d38ed20461
+// import React from 'react'
+// import styled from 'styled-components'
+// import Link from 'gatsby-link'
+
+// import Typography from '@material-ui/core/Typography'
+
+// import ContentComponent from '../components/contentComp'
+// import CardComp from '../components/cardComp'
+// import GridListComp from '../components/gridListComp'
+
+// const IndexPage = ({ children, data }) => (
+// 	<div>
+// 		{/* heading="Who I Am" */}
+// 		<ContentComponent
+// 			heading="Lorem Lorem"
+// 			text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae porro esse fugit, quae facere corrupti eligendi! Possimus atque illo sapiente minus, tempore, illum accusamus animi minima omnis, impedit quasi."
+// 		/>
+
+// 		<ContentComponent
+// 			heading="Lorem Lorem"
+// 			light
+// 			text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae porro esse fugit, quae facere corrupti eligendi! Possimus atque illo sapiente minus, tempore, illum accusamus animi minima omnis, impedit quasi."
+// 		/>
+
+// 		<ContentComponent
+// 			heading="Lorem Lorem"
+// 			text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla molestiae porro esse fugit, quae facere corrupti eligendi! Possimus atque illo sapiente minus, tempore, illum accusamus animi minima omnis, impedit quasi."
+// 		/>
+
+// 		<Typography variant="headline" component="h1">
+// 			Links
+// 		</Typography>
+// 		{data.site.siteMetadata.pageLinks.map((linkName, index) => {
+// 			return (
+// 				<div key={index}>
+// 					{linkName == 'index' && <Link to={`/`}>Home</Link>}
+
+// 					{linkName != 'index' && (
+// 						<Link to={`/${linkName}/`}>
+// 							{linkName} - {index}
+// 						</Link>
+// 					)}
+// 				</div>
+// 			)
+// 		})}
+// 		<Link to="/page-2/">Go to page 2</Link>
+// 		<Link to="/page-3/">Go to page 3</Link>
+// 		<Link to="/ui/">Go to UI</Link>
+// 		<Link to="/material-ui/">Go to Material UI</Link>
+// 		<Link to="/material-ui-2/">Go to Material UI 2</Link>
+// 		<Link to="/typographypage/">Typography</Link>
+// 		<div className="container">
+// 			<div className="row">
+// 				<div className="col-sm-3">column-001</div>
+// 				<div className="col-sm-3">column-002</div>
+// 				<div className="col-sm-3">column-003</div>
+// 				<div className="col-sm-3">column-004</div>
+// 			</div>
+// 		</div>
+// 		<CardComp />
+
+// 		<GridListComp projects={data.site.siteMetadata.projects} />
+// 	</div>
+// )
+
+// export default IndexPage
+
+// export const query = graphql`
+// 	query SitePageLinksProjectsQuery {
+// 		site {
+// 			siteMetadata {
+// 				pageLinks
+// 				projects {
+// 					img
+// 					author
+// 					title
+// 				}
+// 			}
+// 		}
+// 	}
+// `
+
+import React from 'react'
+import Img from 'gatsby-image'
+
+const ImagePage = ({ data }) => (
+	<div>
+		<h1>Hello gatsby-image</h1>
+		{/* <Img /> */}
+		{/* <Img resolutions={data.file.childImageSharp.resolutions} /> */}
+	</div>
+)
+
+export default ImagePage
+
+// export const query = graphql`
+// 	query ImageQuery {
+// 		site {
+// 			siteMetadata {
+// 				pageLinks {
+// 					projects {
+// 						img
+// 						author
+// 						title
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// `
